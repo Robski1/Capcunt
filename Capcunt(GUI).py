880f400a4d0dc5e6ef2ca49895dbd4c7da820726
Robski <robski0000001@gmail.com>
2020-02-13 18:22:10 +0000

    finished

--- Capcunt(GUI).py
+++ Capcunt(GUI).py
@@ -3,7 +3,6 @@ from pynput.keyboard import Key, Controller
 import random
 import threading
 from tkinter import *
-import time
 
 k = Controller()
 numbers={"i":"1","e":"3","a":"4","s":"5","t":"7","g":"9","o":"0"}
@@ -20,30 +19,37 @@ def allFalse():
     global boolcaps
     global boolspecial
     global boolnumbers
+    global lbl3
+    global lbl4
+    global lbl5
     boolcaps = False
     boolspecial = False
     boolnumbers = False
-    print("all false")
+    lbl3.config(text="OFF",font=('ariel', 23))
+    lbl4.config(text="OFF",font=('ariel', 23))
+    lbl5.config(text="OFF",font=('ariel', 23))
 
 def onCaps():
     global boolcaps
+    global lbl3
     boolcaps = True
-    print("boolcaps: ",boolcaps)
+    lbl3.configure(text="ON",font=('ariel', 23))
 
 def onSpecial():
     global boolspecial
+    global lbl4
     boolspecial = True
-    print("special: ",boolspecial)
+    lbl4.config(text="ON",font=('ariel', 23))
 
 def onNumbers():
     global boolnumbers
+    global lbl5
     boolnumbers = True
-    print("numbers: ",boolnumbers)
+    lbl5.config(text="ON",font=('ariel', 23))
 
 def listen():
     with keyboard.Listener(
         on_press=on_press) as listener:
-        print("listener joined")
         listener.join()
 
 def on_press(key):
@@ -91,26 +97,54 @@ def on_press(key):
         except:
             None
 
-#tkinter 
+#window config
 window = Tk()
 window.title("CAPCUNT")
-window.geometry("300x150")
+window.geometry("400x162")
+window.configure(bg="black")
 
+#buttons
 button1 = Button(window, text="CAPS", command=onCaps)
 button1.config(font=('ariel', 15))
-button1.grid(column=1,row=0)
+button1.grid(column=0,row=0,sticky=N+S+E+W)
 
 button2 = Button(window, text="SPECIAL", command=onSpecial)
 button2.config(font=('ariel', 15))
-button2.grid(column=1,row=1)
+button2.grid(column=0,row=1,sticky=N+S+E+W)
 
 button3 = Button(window, text="NUMBERS", command=onNumbers)
 button3.config(font=('ariel', 15))
-button3.grid(column=2,row=0)
+button3.grid(column=0,row=2,sticky=N+S+E+W)
 
 button4 = Button(window, text="STOP", command=allFalse)
 button4.config(font=('ariel', 15))
-button4.grid(column=2,row=1)
+button4.grid(column=0,row=3,sticky=N+S+E+W)
+
+#labels
+lbl=Label(window,text="Caps: ",bg="black",fg="green")
+lbl.config(font=('ariel', 23))
+lbl.grid(column=1, row=0)
+
+lbl1=Label(window,text="   Special: ",bg="black",fg="green")
+lbl1.config(font=('ariel', 23))
+lbl1.grid(column=1, row=1)
+
+lbl2=Label(window,text="      Numbers: ",bg="black",fg="green")
+lbl2.config(font=('ariel', 23))
+lbl2.grid(column=1, row=2)
+
+global lbl3
+global lbl4
+global lbl5
+lbl3=Label(window,text="OFF",bg="black",fg="green")
+lbl3.config(text="OFF",font=('ariel', 23))
+lbl3.grid(column=2, row=0)
+lbl4=Label(window,text="OFF",bg="black",fg="green")
+lbl4.config(text="OFF",font=('ariel', 23))
+lbl4.grid(column=2, row=1)
+lbl5=Label(window,text="OFF",bg="black",fg="green")
+lbl5.grid(column=2, row=2)
+lbl5.config(text="OFF",font=('ariel', 23))
 
 threading.Thread(target=listen).start()
 window.mainloop()
